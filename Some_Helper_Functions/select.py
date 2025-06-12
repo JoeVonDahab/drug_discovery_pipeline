bf371943392a4e8764950011dd37cf06cc631bb7
Shiyang <shiyangxin2@gmail.com>
2025-06-11 21:22:23 -0700

    update

--- Some_Helper_Functions/select.py
+++ Some_Helper_Functions/select.py
@@ -1,17 +1,17 @@
-input_file = r"C:\Users\zhaol\Downloads\drug_discovery_pipeline\Docking Pipeline\results_output\top_40935_ligand_smiles.txt"   # Replace with your actual file name
-output_file = "DiffDock_Top_4000.txt"
+# input_file = r"C:\Users\zhaol\Downloads\drug_discovery_pipeline\Docking Pipeline\results_output\top_40935_ligand_smiles.txt"   # Replace with your actual file name
+# output_file = "DiffDock_Top_4000.txt"
 
-with open(input_file, "r") as f:
-    lines = f.readlines()
+# with open(input_file, "r") as f:
+#     lines = f.readlines()
 
-# Skip the header and extract SMILES strings from tab-separated values
-smiles_lines = [line.strip().split('\t')[1] for line in lines[1:4001] if '\t' in line]
+# # Skip the header and extract SMILES strings from tab-separated values
+# smiles_lines = [line.strip().split('\t')[1] for line in lines[1:4001] if '\t' in line]
 
-# Write SMILES strings to the output file with header
-with open(output_file, "w") as f:
-    f.write("SMILES\n")
-    for smile in smiles_lines:
-        f.write(smile + "\n")
+# # Write SMILES strings to the output file with header
+# with open(output_file, "w") as f:
+#     f.write("SMILES\n")
+#     for smile in smiles_lines:
+#         f.write(smile + "\n")
 
-print(f"{len(smiles_lines)} SMILES strings written to {output_file} with header 'SMILES'")
+# print(f"{len(smiles_lines)} SMILES strings written to {output_file} with header 'SMILES'")
 
